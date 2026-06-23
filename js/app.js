c9ef5396dac2595ee83125b4c90f453422ffe75b
massa-ngl <blaise.nerd01@gmail.com>
2026-06-23 18:29:50 +0100

    refactor: display stats to DOM

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ class CalorieTracker {
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
+
+    document.addEventListener('DOMContentLoaded', this._render.bind(this));
   }
 
   // PUBLIC METHODS/API
@@ -16,6 +18,40 @@ class CalorieTracker {
     this._totalCalories -= workout.calories;
     this._workouts.push(workout);
     this._render();
+  }
+
+  // PRIVATE METHODS
+  _render() {
+    this._displayCalorieLimit();
+    this._displayCaloriesTotal();
+    this._displayCaloriesConsumed();
+    this._displayCaloriesBurned();
+    this._displayCaloriesRemaining();  
+  }
+  _displayCalorieLimit() {
+    const calorieLimitEl = document.querySelector('#calories-limit');
+    calorieLimitEl.innerHTML = this._calorieLimit;
+  }
+  _displayCaloriesTotal() {
+    const totalCaloriesEl = document.querySelector('#calories-total');
+    const consumed = this._meals.map((item) => item.calories).reduce((total, calories) => total + calories, 0);
+    const burned = this._workouts.map((item) => item.calories).reduce((total, calories) => total + calories, 0);
+    totalCaloriesEl.innerHTML = consumed - burned;
+  }
+  _displayCaloriesConsumed() {
+    const caloriesConsumedEl = document.querySelector('#calories-consumed');
+    const consumed = this._meals.map((item) => item.calories).reduce((total, calories) => total + calories, 0);
+    caloriesConsumedEl.innerHTML = consumed;
+  }
+  _displayCaloriesBurned() {
+    const caloriesBurnedEl = document.querySelector('#calories-burned');
+    const burned = this._workouts.map((item) => item.calories).reduce((total, calories) => total + calories, 0);
+    caloriesBurnedEl.innerHTML = burned;
+  }
+  _displayCaloriesRemaining() {
+    const caloriesRemainingEl = document.querySelector('#calories-remaining');
+    const remaining = this._calorieLimit - this._totalCalories;
+    caloriesRemainingEl.innerHTML = remaining;
   }
 }
 
@@ -39,3 +75,10 @@ const lunch = new Meal("Lunch", 700);
 const dinner = new Meal("Dinner", 100);
 const pushups = new Workout("Pushup", 600);
 const sprinting = new Workout("Sprinting", 102);
+
+tracker.addMeal(lunch);
+// tracker.addMeal(dinner);
+
+tracker.addWorkout(pushups);
+// tracker.addWorkout(sprinting);
+console.log(tracker);
