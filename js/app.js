30d26b5080bb855ed4d20460c29b3cda6207a450
massa-ngl <blaise.nerd01@gmail.com>
2026-06-24 19:48:48 +0100

    refactor: add newMeal() & newWorkout()
    Updates the meal and workout tracker forms temporarily

--- js/app.js
+++ js/app.js
@@ -5,18 +5,20 @@ class CalorieTracker {
     this._meals = [];
     this._workouts = [];
 
-    document.addEventListener('DOMContentLoaded', this._render.bind(this));
+    document.addEventListener("DOMContentLoaded", this._render.bind(this));
   }
 
   // PUBLIC METHODS/API
   addMeal(meal) {
     this._totalCalories += meal.calories;
     this._meals.push(meal);
+    this._displayNewMeal(meal);
     this._render();
   }
   addWorkout(workout) {
     this._totalCalories -= workout.calories;
     this._workouts.push(workout);
+    this._displayNewWorkout(workout);
     this._render();
   }
 
@@ -26,57 +28,109 @@ class CalorieTracker {
     this._displayCaloriesTotal();
     this._displayCaloriesConsumed();
     this._displayCaloriesBurned();
-    this._displayCaloriesRemaining();  
+    this._displayCaloriesRemaining();
     this._displayCaloriesProgress();
   }
   _displayCalorieLimit() {
-    const calorieLimitEl = document.querySelector('#calories-limit');
+    const calorieLimitEl = document.querySelector("#calories-limit");
     calorieLimitEl.innerHTML = this._calorieLimit;
   }
   _displayCaloriesTotal() {
-    const totalCaloriesEl = document.querySelector('#calories-total');
-    const consumed = this._meals.map((meal) => meal.calories).reduce((total, calories) => total + calories, 0);
-    const burned = this._workouts.map((workout) => workout.calories).reduce((total, calories) => total + calories, 0);
+    const totalCaloriesEl = document.querySelector("#calories-total");
+    const consumed = this._meals
+      .map((meal) => meal.calories)
+      .reduce((total, calories) => total + calories, 0);
+    const burned = this._workouts
+      .map((workout) => workout.calories)
+      .reduce((total, calories) => total + calories, 0);
     const total = consumed - burned;
     totalCaloriesEl.innerHTML = total;
   }
   _displayCaloriesConsumed() {
-    const caloriesConsumedEl = document.querySelector('#calories-consumed');
-    const consumed = this._meals.map((meal) => meal.calories).reduce((total, calories) => total + calories, 0);
+    const caloriesConsumedEl = document.querySelector("#calories-consumed");
+    const consumed = this._meals
+      .map((meal) => meal.calories)
+      .reduce((total, calories) => total + calories, 0);
     caloriesConsumedEl.innerHTML = consumed;
   }
   _displayCaloriesBurned() {
-    const caloriesBurnedEl = document.querySelector('#calories-burned');
-    const burned = this._workouts.map((workout) => workout.calories).reduce((total, calories) => total + calories, 0);
+    const caloriesBurnedEl = document.querySelector("#calories-burned");
+    const burned = this._workouts
+      .map((workout) => workout.calories)
+      .reduce((total, calories) => total + calories, 0);
     caloriesBurnedEl.innerHTML = burned;
   }
   _displayCaloriesRemaining() {
-    const caloriesRemainingEl = document.querySelector('#calories-remaining');
+    const caloriesRemainingEl = document.querySelector("#calories-remaining");
     const remaining = this._calorieLimit - this._totalCalories;
     caloriesRemainingEl.innerHTML = remaining;
-    const progressEl = document.querySelector('#calorie-progress');
+    const progressEl = document.querySelector("#calorie-progress");
 
     if (remaining <= 0) {
-      caloriesRemainingEl.parentElement.parentElement.classList.remove('bg-light');
-      caloriesRemainingEl.parentElement.parentElement.classList.add('bg-danger');
-      caloriesRemainingEl.style.color = '#fff';
-      caloriesRemainingEl.nextElementSibling.style.color = '#fff';
-      progressEl.classList.remove('bg-success');
-      progressEl.classList.add('bg-danger');
+      caloriesRemainingEl.parentElement.parentElement.classList.remove(
+        "bg-light",
+      );
+      caloriesRemainingEl.parentElement.parentElement.classList.add(
+        "bg-danger",
+      );
+      caloriesRemainingEl.style.color = "#fff";
+      caloriesRemainingEl.nextElementSibling.style.color = "#fff";
+      progressEl.classList.remove("bg-success");
+      progressEl.classList.add("bg-danger");
     } else {
-      caloriesRemainingEl.parentElement.parentElement.classList.add('bg-light');
-      caloriesRemainingEl.parentElement.parentElement.classList.remove('bg-danger');
-      caloriesRemainingEl.style.color = '#000';
-      caloriesRemainingEl.nextElementSibling.style.color = '#000';
-      progressEl.classList.remove('bg-danger');
-      progressEl.classList.add('bg-success');
+      caloriesRemainingEl.parentElement.parentElement.classList.add("bg-light");
+      caloriesRemainingEl.parentElement.parentElement.classList.remove(
+        "bg-danger",
+      );
+      caloriesRemainingEl.style.color = "#000";
+      caloriesRemainingEl.nextElementSibling.style.color = "#000";
+      progressEl.classList.remove("bg-danger");
+      progressEl.classList.add("bg-success");
     }
   }
   _displayCaloriesProgress() {
-    const progressEl = document.querySelector('#calorie-progress');
+    const progressEl = document.querySelector("#calorie-progress");
     const percentage = (this._totalCalories / this._calorieLimit) * 100;
     const width = Math.min(percentage, 100);
     progressEl.style.width = `${width}%`;
+  }
+  _displayNewMeal(meal) {
+    const mealsEl = document.querySelector("#meal-items");
+    const mealEl = document.createElement("div");
+    mealEl.classList.add("card", "my-2");
+    mealEl.innerHTML = `
+      <div class="card-body">
+        <div class="d-flex align-items-center justify-content-between">
+          <h4 class="mx-1">${meal.name}</h4>
+          <div class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5">
+            ${meal.calories}
+          </div>
+          <button class="delete btn btn-danger btn-sm mx-2">
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+        </div>
+      </div>`;
+    
+      mealsEl.appendChild(mealEl);
+  }
+  _displayNewWorkout(workout) {
+    const workoutsEl = document.querySelector("#workout-items");
+    const workoutEl = document.createElement("div");
+    workoutEl.classList.add("card", "my-2");
+    workoutEl.innerHTML = `
+      <div class="card-body">
+        <div class="d-flex align-items-center justify-content-between">
+          <h4 class="mx-1">${workout.name}</h4>
+          <div class="fs-1 bg-secondary text-white text-center rounded-2 px-2 px-sm-5">
+            ${workout.calories}
+          </div>
+          <button class="delete btn btn-danger btn-sm mx-2">
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+        </div>
+      </div>`;
+    
+    workoutsEl.appendChild(workoutEl);
   }
 }
 
@@ -99,34 +153,38 @@ class App {
   constructor() {
     this._tracker = new CalorieTracker();
 
-  this._render();
+    this._render();
   }
-  
+
   _render() {
-    document.querySelector('#meal-form').addEventListener('submit', this._newItem.bind(this, 'meal'));
-    document.querySelector('#workout-form').addEventListener('submit', this._newItem.bind(this, 'workout'));
+    document
+      .querySelector("#meal-form")
+      .addEventListener("submit", this._newItem.bind(this, "meal"));
+    document
+      .querySelector("#workout-form")
+      .addEventListener("submit", this._newItem.bind(this, "workout"));
   }
   _newItem(type, e) {
     e.preventDefault();
     let name = document.querySelector(`#${type}-name`);
     let calories = document.querySelector(`#${type}-calories`);
 
-    if (name.value === '' || calories.value === '') {
-      alert('Please enter values in all fields');
+    if (name.value === "" || calories.value === "") {
+      alert("Please enter values in all fields");
       return;
     }
 
-    if (type === 'meal') {
+    if (type === "meal") {
       const meal = new Meal(name.value, +calories.value);
       this._tracker.addMeal(meal);
     }
-    if(type === 'workout') {
+    if (type === "workout") {
       const workout = new Workout(name.value, +calories.value);
       this._tracker.addWorkout(workout);
     }
 
-    name.value = '';
-    calories.value = '';
+    name.value = "";
+    calories.value = "";
 
     const collapseItem = document.querySelector(`#collapse-${type}`);
     const bsCollapse = new bootstrap.Collapse(collapseItem, {
