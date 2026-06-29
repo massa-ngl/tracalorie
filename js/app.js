1a89c1c2a5bf534daecf57a64550bcf90c5644b6
massa-ngl <blaise.nerd01@gmail.com>
2026-06-29 21:55:06 +0100

    feat: persist workouts to localStorage
    This will set up a array of objects, workouts, in the browser's localStorage. New workouts are appended to the workouts array for it to be displayed on the DOM persistently. Upon page refresh or new same page session creation, the the workouts items will be retrieved from the localStorage and displayed on the DOM incase these data weren't cleared at all.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ class CalorieTracker {
     this._calorieLimit = Storage.getCalorieLimit();
     this._totalCalories = Storage.getTotalCalories(0);
     this._meals = Storage.getMeals();
-    this._workouts = [];
+    this._workouts = Storage.getWorkouts();
 
     document.addEventListener("DOMContentLoaded", this._render.bind(this));
   }
@@ -18,9 +18,11 @@ class CalorieTracker {
     this._render();
   }
   addWorkout(workout) {
-    this._totalCalories -= workout.calories;
     this._workouts.push(workout);
+    this._totalCalories -= workout.calories;
     Storage.updateCalories(this._totalCalories);
+    this._displayNewWorkout(workout);
+    Storage.saveWorkout(workout);
     this._render();
   }
   removeMeal(id) {
@@ -57,6 +59,7 @@ class CalorieTracker {
   }
   loadItems() {
     this._meals.forEach((meal) => this._displayNewMeal(meal));
+    this._workouts.forEach((workout) => this._displayNewWorkout(workout));
   }
 
   // PRIVATE METHODS
@@ -231,6 +234,22 @@ class Storage {
     const meals = Storage.getMeals();
     meals.push(meal);
     localStorage.setItem('meals', JSON.stringify(meals));
+  }
+
+  static getWorkouts() {
+    let workouts;
+    if (localStorage.getItem('workouts') === null) {
+      workouts = [];
+    } else {
+      workouts = JSON.parse(localStorage.getItem('workouts'));
+    }
+    return workouts;
+  }
+
+  static saveWorkout(workout) {
+    const workouts = Storage.getWorkouts();
+    workouts.push(workout);
+    localStorage.setItem('workouts', JSON.stringify(workouts));
   }
 }
 
