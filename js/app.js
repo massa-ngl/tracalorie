cf7ff8b6fbdfbf66a1bc186cadbed3ca3bbdde84
massa-ngl <blaise.nerd01@gmail.com>
2026-06-29 22:58:31 +0100

    feat: reset stats and items
    This feature sets the total calories, meals list, & workouts list back to their original states, 0 and empty lists, respectively. Other features like remove meals and remove workouts will ensure no stats or items are displayed on the DOM after after the Reset button has been clicked.

--- js/app.js
+++ js/app.js
@@ -52,6 +52,7 @@ class CalorieTracker {
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
+    Storage.clearAll();
     this._render();
   }
   setLimit(calorieLimit) {
@@ -273,6 +274,12 @@ class Storage {
       }
     });
     localStorage.setItem("workouts", JSON.stringify(workouts));
+  }
+
+  static clearAll() {
+    localStorage.removeItem('totalCalories');
+    localStorage.removeItem('meals');
+    localStorage.removeItem('workouts');
   }
 }
 
